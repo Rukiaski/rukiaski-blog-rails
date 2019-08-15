27dd7f509ad2ee207bd001a0640b99e9ae2b690a
Rukiaski <thamiresaraujoml@gmail.com>
2019-08-15 15:46:27 -0300

    Add passord to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_154350) do
+ActiveRecord::Schema.define(version: 2019_08_15_183845) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_08_15_154350) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
